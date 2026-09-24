3aefecdf6be0a7ec44c8da232e1a921626e8defc
Ebru Hızlı <delaglace1995@gmail.com>
2026-09-24 21:31:24 +0300

    chore: restore shared files

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,5 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
-
-import StatisticsTab from './pages/StatisticsTab/StatisticsTab';
-import CurrencyTab from './pages/CurrencyTab/CurrencyTab';
-
 const App = () => {
-  return (
-    <Routes>
-      <Route path="/statistics" element={<StatisticsTab />} />
-      <Route path="/currency" element={<CurrencyTab />} />
-
-      <Route
-        path="*"
-        element={<Navigate to="/statistics" replace />}
-      />
-    </Routes>
-  );
+  return <div>Money Guard</div>;
 };
 
 export default App;
--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,7 +1,1 @@
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-    baseURL: 'BURAYA_BACKEND_ADRESI',
-});
-
-export default axiosInstance;
+// TODO: implement
