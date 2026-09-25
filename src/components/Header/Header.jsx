29648780997ee7b9ba4b402636e6f4ebfcaf9aee
leogos <olsenaleo@gmail.com>
2026-09-25 23:00:13 +0300

    fix registration background and project config

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,22 @@
-import { useSelector } from "react-redux";
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { selectUser } from "../../redux/auth/selectors";
-import LogoutModal from "../LogoutModal/LogoutModal";
-import { IoIosLogOut } from "react-icons/io";
-import styles from "./Header.module.css";
+import { useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { selectUser } from '../../redux/auth/selectors';
+import LogoutModal from '../LogoutModal/LogOutModal';
+import { IoIosLogOut } from 'react-icons/io';
+import styles from './Header.module.css';
 
 const Header = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const user = useSelector(selectUser);
-  const username = user?.email?.split("@")[0] || "User";
+  const username = user?.email?.split('@')[0] || 'User';
 
   useEffect(() => {
-    const handleEsc = (e) => {
-      if (e.key === "Escape") setIsModalOpen(false);
+    const handleEsc = e => {
+      if (e.key === 'Escape') setIsModalOpen(false);
     };
-    if (isModalOpen) window.addEventListener("keydown", handleEsc);
-    return () => window.removeEventListener("keydown", handleEsc);
+    if (isModalOpen) window.addEventListener('keydown', handleEsc);
+    return () => window.removeEventListener('keydown', handleEsc);
   }, [isModalOpen]);
 
   return (
